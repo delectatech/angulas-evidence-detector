4ed891fb0b23cdb2b77acbabc08ec31cd12b9700
Mario Carpallo <mcarpallo@delectatech.com>
2025-06-30 12:51:02 +0200

    feat: update dependencies and add new assets

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,24 @@
 import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import Aura from '@primeng/themes/aura';  // este sí existe
 import { routes } from './app.routes';
 import { HttpClientModule, provideHttpClient, withInterceptors } from '@angular/common/http';
 import { BrowserAnimationsModule, provideAnimations } from '@angular/platform-browser/animations';
 import { MessageService } from 'primeng/api';
 import { AuthInterceptor } from './auth/services/authInterceptor.service';
 import { LOCALE_ID } from '@angular/core';
+import { providePrimeNG } from 'primeng/config';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     importProvidersFrom(HttpClientModule),
+     providePrimeNG({
+    theme: {
+      preset: Aura
+    }
+  }),
     provideAnimations(),
     MessageService,    { provide: LOCALE_ID, useValue: 'es' },
     importProvidersFrom(BrowserAnimationsModule),
